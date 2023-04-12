6d610c58286aa5706c754be813f3b65f58619305
Gabriel Castro <c4str0@live.com>
2023-04-12 09:31:16 -0300

    feat: Melhorias na documentação da API engine

--- vitruvio.d.ts
+++ vitruvio.d.ts
@@ -1,15 +1,30 @@
 declare class Engine {
     /**
-     * Debug method for logging messages.
-     * @param message - The message to log.
+     * Método debug para log de mensagens no vitrúvio
+     * @param message - A mensagem a ser logada pelo vitrúvio
      */
     debug(message: string): void;
     /**
-     * Deprecated method.
-     * @deprecated Use otherMethod() instead.
+     * Método OBSOLETO. Não utilizar
+     * @deprecated Não utilize esta função. Use a lib messages ao invés disso
      */
-    deprecatedMethod(): void;
-    // Add other methods and properties as needed
+    funcaoObsoleta(): void;
+    /**
+     * Método para obter o valor de um campo do Vitrúvio
+     * @param field - O nome do campo a ser obtido - ex: "dbtUsuarios"
+     * @return - O objeto do campo, ou undefined caso não exista
+     */
+    getField(field:String):Object;
+    /**
+     * Método para obter o numero da instância do processo em execução no Vitrúvio
+     * @return - O número da instância do processo em execução no Vitrúvio
+     * @example
+     * ```javascript
+     * // Retorna o ID do Processo do vitrúvio
+     * const processInstanceId = engine.getProcessInstanceId();
+     * ```
+     */
+    getProcessInstanceId():number;
 }
 // Declare the global `engine` object as an instance of the `Engine` class
 declare const engine: Engine;
