fdfd0d42684cf58de14e321aaf81af5188c8c1ad
Gabriel Castro <c4str0@live.com>
2023-04-14 11:12:09 -0300

    feat(libService): Iniciada documentação da API libService

---
+++ types/LibService.d.ts
@@ -0,0 +1,10 @@
+type ScriptMapping = {
+  'db': typeof QueryService,
+  'messages': typeof MessageService,
+};
+
+declare class libService {
+  static loadScript<T extends keyof ScriptMapping>(scriptName: T): ScriptMapping[T];
+}
+
+declare const lib: libService;
